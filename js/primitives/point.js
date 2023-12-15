c0b165c2a9669d5c0ed3c55d594cb1fcdc859984
RuriEnciso <rurienciso@gmail.com>
2023-12-15 00:12:37 -0500

    implementing basic functions

--- js/primitives/point.js
+++ js/primitives/point.js
@@ -1,7 +1,11 @@
-class point {
+class Point {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+    }
+
+    equals(point) {
+        return this.x === point.x && this.y === point.y;
     }
 
     draw(ctx, size = 18, color = "black") {
